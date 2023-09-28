48afbb32ace4664bc8b1a506237c13059275124d
Tomas Marin <marintomas90@gmail.com>
2023-09-28 09:28:40 -0500

    v1.1

--- src/com/juegoPPT/dominio/Jugada.java
+++ src/com/juegoPPT/dominio/Jugada.java
@@ -1,27 +1,27 @@
 package com.juegoPPT.dominio;
 
 public class Jugada {
-    private int puntajeMaquina;
-    private int puntajeJugador;
-    public int getPuntajeMaquina() {
-        return puntajeMaquina;
-    }
-    public int getPuntajeJugador() {
-        return puntajeJugador;
-    }
+    private static int puntajeMaquina;
+    private static int puntajeJugador;
     public static String comprobarJugada(String jugadaMaquina, String jugadaJugador, int jugador) {
         String [] players = {"Maquina", "Jugador"};
         boolean comprobar = (jugador != 0); //Si el jugador es 0, bool False <--> Si es 1, bool True
         if (jugadaMaquina.equals(jugadaJugador)) {
+            puntajeMaquina++;
+            puntajeJugador++;
             return "Empate";
         } else if ((jugadaMaquina.equals("Piedra") && jugadaJugador.equals("Tijeras")) ||
                 (jugadaMaquina.equals("Papel") && jugadaJugador.equals("Piedra")) ||
                 (jugadaMaquina.equals("Tijeras") && jugadaJugador.equals("Papel"))) {
-            if (comprobar){
+            if (comprobar){ //Define a la maquina como ganadora
+                puntajeMaquina++;
+                puntajeMaquina++;
                 return  players[0] + " escogió --> " + jugadaJugador + "\n" +
                         players[jugador] + " escogió --> " + jugadaMaquina + "\n"
                         +"Ganó " + players[jugador] + " con la jugada " + jugadaMaquina;
-            }else{
+            }else{ //Define al jugador como ganador
+                puntajeJugador++;
+                puntajeJugador++;
                 return  players[jugador] + " escogió --> " + jugadaMaquina + "\n" +
                         players[1] + " escogió --> " + jugadaJugador + "\n"
                         +"Ganó " + players[jugador] + " con la jugada " + jugadaMaquina;
@@ -30,5 +30,10 @@ public class Jugada {
             return comprobarJugada(jugadaJugador, jugadaMaquina, 1);
         }
     }
-
+    public static int getPuntajeMaquina() {
+        return puntajeMaquina;
+    }
+    public static int getPuntajeJugador() {
+        return puntajeJugador;
+    }
 }
--- src/com/juegoPPT/dominio/PPT.java
+++ src/com/juegoPPT/dominio/PPT.java
@@ -17,13 +17,15 @@ public class PPT {
         return opcion;
     }
     public static void instrucciones(){
-        JOptionPane.showMessageDialog(null, "El objetivo es vencer al oponente seleccionando el arma ganadora según las siguientes reglas:\n" +
+        JOptionPane.showMessageDialog(null,
+                "El objetivo es vencer al oponente seleccionando el arma ganadora según las siguientes reglas:\n" +
                 "\n" +
-                "La piedra aplasta la tijera. (Gana la piedra.)\n" +
-                "La tijera corta el papel. (Gana la tijera.)\n" +
-                "El papel envuelve la piedra. (Gana el papel.)\n" +
+                "La piedra aplasta la tijera. (Gana la piedra y obtiene 2 puntos.)\n" +
+                "La tijera corta el papel. (Gana la tijera y obtiene 2 puntos.)\n" +
+                "El papel envuelve la piedra. (Gana el papel y obtiene 2 puntos.)\n" +
                 "\n"+
-                "En caso de empate (que dos jugadores elijan el mismo elemento o que tres jugadores elijan cada uno un objeto distinto), se juega otra vez.");
+                "En caso de empate (Ambos obtienen 1 punto)."
+                , "PIEDRA, PAPEL O TIJERA",JOptionPane.INFORMATION_MESSAGE, icon);
     }
     public static void jugar(){
         String[] options = { "Piedra", "Papel", "Tijeras" };
@@ -33,6 +35,23 @@ public class PPT {
         JOptionPane.showMessageDialog(null, Jugada.comprobarJugada(options[seleccion], jugador, 0), "PIEDRA, PAPEL O TIJERA", JOptionPane.INFORMATION_MESSAGE,icon);
     }
     public static void resultados(){
-        JOptionPane.showMessageDialog(null, "Proximamente...");
+        System.out.println(Jugada.getPuntajeMaquina());
+        System.out.println(Jugada.getPuntajeJugador());
+        JOptionPane.showMessageDialog(null,
+                "Los resultados son" + "\n"
+                +"La Maquina obtuvo --> " + Jugada.getPuntajeMaquina() + "\n"
+                +"El Jugador obtuvo --> " + Jugada.getPuntajeJugador() + "\n"
+                +ganador(Jugada.getPuntajeMaquina(),Jugada.getPuntajeJugador())
+                , "PIEDRA, PAPEL O TIJERA", JOptionPane.INFORMATION_MESSAGE,icon);
     }
+    public static String ganador(int puntajeMaquina, int puntajeJugador) {
+        if (puntajeMaquina == puntajeJugador) {
+            return "La Maquina y el Jugador quedaron empatados";
+        } else if (puntajeMaquina > puntajeJugador) {
+            return "La Maquina quedó ganadora";
+        } else {
+            return "El Jugador quedó ganador";
+        }
+    }
+
 }
